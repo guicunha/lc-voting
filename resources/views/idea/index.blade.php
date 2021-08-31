a4d6d386167b43d3c3fd7bcc0925b3c0da0359c7
Guilherme Cunha <guilherme@alugoaki.com.br>
2021-08-31 11:13:47 -0300

    click event on cards

--- resources/views/idea/index.blade.php
+++ resources/views/idea/index.blade.php
@@ -30,7 +30,20 @@ placeholder-gray-900 border-none">
 
     <div class="ideas-container space-y-6 my-6">
         @foreach($ideas as $idea)
-        <div class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex">
+        <div
+            x-data
+            @click="
+                const clicked = $event.target
+                const target = clicked.tagName.toLowerCase()
+                const ignores = ['button','svg','path','a'];
+
+                if(! ignores.includes(target)) {
+                    clicked.closest('.idea-container').querySelector('.idea-link').click()
+                }
+                {{--$event.target.closest('.idea-container').querySelector('.idea-link').click()--}}
+            "
+            class="idea-container hover:shadow-card transition duration-150
+        ease-in bg-white rounded-xl flex cursor-pointer">
             <div class="border-r border-gray-100 px-5 py-8">
                 <div class="text-center">
                     <div class="font-semibold text-2xl">12</div>
@@ -52,7 +65,7 @@ placeholder-gray-900 border-none">
 
                 <div class="w-full flex flex-col justify-between mx-4">
                     <h4 class="text-xl font-semibold">
-                        <a href="{{route('idea.show', $idea)}}" class="hove:underline">{{$idea->title}}</a>
+                        <a href="{{route('idea.show', $idea)}}" class="idea-link hove:underline">{{$idea->title}}</a>
                     </h4>
                     <div class="text-gray-600 mt-3 line-clamp-3">
                         {{ $idea->description }}
